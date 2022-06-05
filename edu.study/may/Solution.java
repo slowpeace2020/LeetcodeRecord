65c8f8cf9b3c66a822c132aedeaa0a01c7be11f7
slowpeace2020 <slowpeace2020@gmail.com>
2022-06-05 18:20:27 -0400

    leetcode exercise prefixsum

--- edu.study/may/Solution.java
+++ edu.study/may/Solution.java
@@ -197,14 +197,246 @@ public class Solution {
         return max;
     }
 
+    public int sumRootToLeaf(TreeNode root) {
+        return sumRootToLeaf(root,0);
+    }
+
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+
+    public int sumRootToLeaf(TreeNode root,int pre) {
+        if(root==null){
+            return pre;
+        }
+
+        if(root.left==null&&root.right==null){
+            System.out.println(root.val+2*pre);
+            System.out.println(root.val);
+            return root.val+2*pre;
+        }
+
+        if(root.left==null){
+            return sumRootToLeaf(root.right,root.val+2*pre);
+        }
+
+        if(root.right==null){
+            return sumRootToLeaf(root.left,root.val+2*pre);
+        }
+        return sumRootToLeaf(root.right,root.val+2*pre)+sumRootToLeaf(root.right,root.val+2*pre);
+    }
+
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        if(n<=2){
+            return false;
+        }
+        Stack<Integer> stack = new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            if(!stack.isEmpty()&&stack.peek()>=nums[i]){
+                if(stack.size()>=2){
+                    int max =stack.pop();
+                    int min = max;
+                    while(!stack.isEmpty()){
+                        min = stack.pop();
+                        if(min>nums[i]){
+                            return true;
+                        }
+                    }
+
+                    stack.push(min);
+                    stack.push(max);
+                }
+
+            }else{
+                stack.push(nums[i]);
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public int minOperations(int[] nums1, int[] nums2) {
+        int target = 0;
+        int sum = 0;
+
+        for (int num : nums1) {
+            sum += num;
+        }
+
+        for (int num : nums2) {
+            target += num;
+        }
+
+        if (target == sum) {
+            return 0;
+        }
+
+        Arrays.sort(nums1);
+        int res = 0;
+        if (target < sum) {
+            int count = sum - target;
+            for (int i = nums1.length - 1; i >= 0; i--) {
+
+                res++;
+                count -= nums1[i] - 1;
+                if (count <= 0) {
+                    return res;
+                }
+            }
+
+        } else {
+            int count = target - sum;
+            for (int i = 0; i < nums1.length; i++) {
+                res++;
+                count -= 6 - nums1[i];
+                if (count <= 0) {
+                    return res;
+                }
+            }
+
+        }
+
+        return -1;
+
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-//        solution.reorderList(head);
+//        TreeNode root = new TreeNode(1);
+//        root.left = new TreeNode(0);
+//        root.left.left = new TreeNode(0);
+//        root.left.right = new TreeNode(1);
+//        root.right = new TreeNode(1);
+//        root.right.left = new TreeNode(0);
+//        root.right.right = new TreeNode(1);
+//        solution.sumRootToLeaf(root);
 
-        solution.largestRectangleArea(new int[]{2,1,2});
+//        solution.largestRectangleArea(new int[]{2,1,2});
+
+        solution.minOperations(new int[]{1,2,3,4,5,6},new int[]{1,1,2,2,2,2});
+    }
+
+    public String removeOuterParentheses(String S) {
+        StringBuilder s = new StringBuilder();
+        int opened = 0;
+        for (char c : S.toCharArray()) {
+            if (c == '(' && opened++ > 0) s.append(c);
+            if (c == ')' && opened-- > 1) s.append(c);
+        }
+        return s.toString();
+    }
+
+    public int leastBricks(List<List<Integer>> wall) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(List<Integer> list:wall){
+            int sum =0;
+            for(int i=0;i<list.size()-1;i++){
+                sum+=list.get(i);
+                map.put(sum,map.getOrDefault(sum,0)+1);
+            }
+        }
+
+        int count = 0;
+        for(int val:map.values()){
+            count = Math.max(count,val);
+        }
+
+        return wall.size()-count;
+    }
+
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        LinkedList<TreeNode> nodes = new LinkedList<>();
+        List<Integer> res = new ArrayList<>();
+        if(canFind(root,target,nodes)){
+            int len = nodes.size();
+            //not over the distance
+            for(int i=len-1;i>=len-1-k&&i>=0;i--){
+                int distance = len-1-i;//current distance
+                //keep search leftover distance
+                //avoid repeate path by nodes
+                search(res,nodes,k-distance,nodes.get(i));
+
+            }
+        }
+
+        return res;
+    }
+
+    //judge if could find the node, and list record the path from root to target
+    public boolean canFind(TreeNode root, TreeNode target,LinkedList<TreeNode> list){
+        if(root==null){
+            return false;
+        }
+        if(root.val==target.val){
+            list.add(root);
+            return true;
+        }
+
+        //recursion find target
+        if(canFind(root.left,target,list)||canFind(root.right,target,list)){
+            //add parent node
+            list.addFirst(root);
+            return true;
+        }
+
+        return false;
+
+    }
+
+    //from parent node to child node with distance
+    private void search(List<Integer> list,List<TreeNode> visited,int distance,TreeNode root){
+        if(root==null){
+            return;
+        }
+
+        if(distance==0){
+            list.add(root.val);
+            return;
+        }
+
+        if(!visited.contains(root.left)){
+            visited.add(root.left);
+            search(list,visited,distance-1,root.left);
+        }
+        if(!visited.contains(root.right)){
+            visited.add(root.right);
+            search(list,visited,distance-1,root.right);
+        }
+
+    }
+
+    public int[] getAverages(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n];
+        long windowSum = 0;
+        Arrays.fill(res,-1);
+        for(int i=0;i<2*k&&i<n;i++){
+            windowSum+=nums[i];
+        }
+
+        int start = 0;
+        for(int i=2*k;i<n;i++){
+            windowSum+=nums[i];
+            if(i>2*k){
+                windowSum-=nums[i-2*k-1];
+            }
+            res[i-k]=(int)(windowSum/(2*k+1));
+        }
+
+        return res;
     }
 }
